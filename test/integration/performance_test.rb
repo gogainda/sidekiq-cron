b7d3213b0ce286f0ecadc3f41c470328314edf7f
Igor Victor <gogainda@yandex.ru>
2022-09-08 18:12:06 +0200

    Add TruffleRuby support

--- test/integration/performance_test.rb
+++ test/integration/performance_test.rb
@@ -44,10 +44,10 @@ describe 'Performance Poller' do
     end
 
     puts "Performance test finished in #{bench.real}"
-    if RUBY_ENGINE == 'truffleruby'
+    if RUBY_ENGINE == 'truffleruby' 
       puts "TODO Don't skip this test on TruffleRuby, it's fast enough to pass it" if bench.real < 50
       skip "TruffleRuby is slower than CRuby at this moment"
-    else
+    end
 
     assert_operator bench.real, :<, 50
   end
